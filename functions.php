0332fc174fd95a7dfdc19e57dbd130bdf20b8f38
Mark Marzeotti <mark@markmarzeotti.com>
2019-08-09 07:15:44 -0400

    additional styles and added agency logos to work posts

--- functions.php
+++ functions.php
@@ -9,19 +9,19 @@
  * Sets up theme defaults and registers support for various WordPress features.
  */
 function marzeotti_portfolio_setup() {
-    /*
-     * Register menu locations.
-     *
-     * @link https://developer.wordpress.org/reference/functions/register_nav_menus/
-     */
-    register_nav_menus( array(
-        'button-menu' => esc_html__( 'Button Menu', 'marzeotti-portfolio' ),
-    ) );
+	/*
+	 * Register menu locations.
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/register_nav_menus/
+	 */
+	register_nav_menus( array(
+		'button-menu' => esc_html__( 'Button Menu', 'marzeotti-portfolio' ),
+	) );
 
-    /**
-     * Overide medium size to crop.
-     */
-    add_image_size( 'medium', 600, 460, true );
+	/**
+	 * Overide medium size to crop.
+	 */
+	add_image_size( 'medium', 600, 460, true );
 }
 add_action( 'after_setup_theme', 'marzeotti_portfolio_setup' );
 
@@ -39,26 +39,26 @@ add_action( 'wp_enqueue_scripts', 'marzeotti_portfolio_scripts' );
  * Add custom post types.
  */
 function marzeotti_portfolio_post_types( $post_types ) {
-    return array(
-        array( 
-            'slug'          => 'talk', 
-            'url_base'      => 'talks', 
-            'name_singular' => 'Talk', 
-            'name_plural'   => 'Talks', 
-            'icon'          => 'dashicons-format-status',
-            'taxonomies'    => array(),
-            'has_archive'   => false 
-        ),
-        array( 
-            'slug'          => 'work', 
-            'url_base'      => 'work', 
-            'name_singular' => 'Work', 
-            'name_plural'   => 'Work', 
-            'icon'          => 'dashicons-editor-code',
-            'taxonomies'    => array(),
-            'has_archive'   => false 
-        )
-    );
+	return array(
+		array( 
+			'slug'          => 'talk', 
+			'url_base'      => 'talks', 
+			'name_singular' => 'Talk', 
+			'name_plural'   => 'Talks', 
+			'icon'          => 'dashicons-format-status',
+			'taxonomies'    => array(),
+			'has_archive'   => false 
+		),
+		array( 
+			'slug'          => 'work', 
+			'url_base'      => 'work', 
+			'name_singular' => 'Work', 
+			'name_plural'   => 'Work', 
+			'icon'          => 'dashicons-editor-code',
+			'taxonomies'    => array(),
+			'has_archive'   => false 
+		)
+	);
 }
 add_filter( 'marzeotti_base_custom_post_types', 'marzeotti_portfolio_post_types' );
 
@@ -66,16 +66,16 @@ add_filter( 'marzeotti_base_custom_post_types', 'marzeotti_portfolio_post_types'
  * Add custom taxonomies.
  */
 function marzeotti_portfolio_taxonomies( $taxonomies ) {
-    return array(
-        array( 
-            'slug'              => 'agency', 
-            'url_base'          => 'agency', 
-            'name_singular'     => 'Agency', 
-            'name_plural'       => 'Agencies', 
-            'post_types'        => array( 'work' ),
-            'show_admin_column' => true 
-        )
-    );;
+	return array(
+		array( 
+			'slug'              => 'agency', 
+			'url_base'          => 'agency', 
+			'name_singular'     => 'Agency', 
+			'name_plural'       => 'Agencies', 
+			'post_types'        => array( 'work' ),
+			'show_admin_column' => true 
+		)
+	);
 }
 add_filter( 'marzeotti_base_custom_taxonomies', 'marzeotti_portfolio_taxonomies' );
 
@@ -83,12 +83,52 @@ add_filter( 'marzeotti_base_custom_taxonomies', 'marzeotti_portfolio_taxonomies'
  * Overwrite default functionality for byline.
  */
 function marzeotti_base_posted_by() {
-    $byline = sprintf(
-        /* translators: %s: post author. */
-        esc_html_x( 'by %s', 'post author', 'marzeotti-base' ),
-        '<span class="author"><a href="' . esc_url( home_url( '/' ) ) . 'about/">' . esc_html( get_the_author() ) . '</a></span>'
-    );
+	$byline = sprintf(
+		/* translators: %s: post author. */
+		esc_html_x( 'by %s', 'post author', 'marzeotti-base' ),
+		'<span class="author"><a href="' . esc_url( home_url( '/' ) ) . 'about/">' . esc_html( get_the_author() ) . '</a></span>'
+	);
 
-    echo '<span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
-
+	echo '<span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
 }
+
+/**
+ * Adds optimal image sizes for sharing on Facebook and Twitter.
+ */
+function marzeotti_portfolio_add_share_image_sizes() {
+	add_image_size( 'facebook-share', 1200, 630, true );
+	add_image_size( 'twitter-share', 1024, 512, true );
+}
+add_action( 'after_setup_theme', 'marzeotti_portfolio_add_share_image_sizes' );
+
+/**
+ * Sets image size for Yoast to use for Facebook shares.
+ */
+function marzeotti_portfolio_set_yoast_facebook_share_image_size() {
+	return 'facebook-share';
+}
+add_filter( 'wpseo_opengraph_image_size', 'marzeotti_portfolio_set_yoast_facebook_share_image_size' );
+
+/**
+ * Sets image size for Yoast to use for Twitter shares.
+ */
+function marzeotti_portfolio_set_yoast_twitter_share_image_size() {
+	return 'twitter-share';
+}
+add_filter( 'wpseo_twitter_image_size', 'marzeotti_portfolio_set_yoast_twitter_share_image_size' );
+
+/**
+ * Add post taxonomies to Archive Content posts.
+ */
+function marzeotti_portfolio_archive_content_after_title() {
+	$content = '';
+	$terms = get_the_terms( $post, 'agency' );
+	if ( ! empty( $terms ) ) {
+		foreach ( $terms as $term ) {
+			$agency_logo_id = get_field( 'logo', $term );
+			$content .= '<span class="agency-logo">' . wp_get_attachment_image( $agency_logo_id, 'thumbnail', false, array( 'alt' => $term->name . ' logo', 'title' => 'This project was completed for ' . $term->name . '.' ) ) . '</span>';
+		}
+	}
+	return $content;
+}
+add_filter( 'archive_content_after_title', 'marzeotti_portfolio_archive_content_after_title' );
