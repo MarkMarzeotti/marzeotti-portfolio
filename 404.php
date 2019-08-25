15ca62d6280a4451978636713d58a30b8905070d
Mark Marzeotti <mark@markmarzeotti.com>
2019-08-25 16:42:05 -0400

    built search and 404

---
+++ 404.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package Marzeotti_Base
+ */
+
+get_header();
+?>
+
+	<div id="main" class="content__blocks">
+
+		<div class="wp-block-portfolio-blocks-page-heading">
+			<div class="page-heading">
+				<div class="page-heading__content">
+					<span>404</span>
+					<h1><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'marzeotti-base' ); ?></h1>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="content__single">
+		<article>
+			<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try searching?', 'marzeotti-base' ); ?></p>
+			<?php get_search_form(); ?>
+		</article>
+	</div>
+
+<?php
+get_footer();
---
+++ search.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * The template for displaying search results pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ *
+ * @package Marzeotti_Base
+ */
+
+get_header();
+?>
+
+	<main id="main" class="content__blocks">
+
+		<div class="wp-block-portfolio-blocks-page-heading">
+			<div class="page-heading">
+				<div class="page-heading__content">
+					<span>Search</span>
+					<h1>
+						<?php
+						/* translators: %s: search query. */
+						printf( esc_html__( 'Search Results for: %s', 'marzeotti-base' ), get_search_query() );
+						?>
+					</h1>
+				</div>
+			</div>
+		</div>
+		
+		<div class="wp-block-portfolio-blocks-archive-content">
+			<div class="archive-content">
+				<div class="archive-content__posts wide">
+
+					<?php 
+					if ( have_posts() ) :
+						while ( have_posts() ) : the_post(); 
+							?>
+				
+							<div class="archive-content__item">
+								<div class="archive-content__content">
+									<h2 class="h3"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+									<?php the_excerpt(); ?>
+									<a class="archive-content__button" href="<?php the_permalink(); ?>">Read More</a>
+								</div>
+							</div>
+
+							<?php
+						endwhile;
+
+						the_posts_navigation();
+
+					else :
+						?>
+						
+						<article>
+							<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try searching?', 'marzeotti-base' ); ?></p>
+							<?php get_search_form(); ?>
+						</article>
+
+						<?php
+					endif;
+					?>
+
+				</div>
+			</div>
+		</div>
+
+	</main>
+
+<?php
+get_footer();
